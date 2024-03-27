58ea31b1428bd24c06d2e3dbe85e49d773d2540d
Freddyd95 <freddydiaz28@hotmail.com>
2024-03-26 23:52:48 -0700

    New Places can be added

---
+++ models/places.js
@@ -0,0 +1,13 @@
+module.exports = [{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: '/images/Thai-Restaurant.jpg'
+  }, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: '/images/Cafe.jpg'
+  }]
