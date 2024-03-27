6a76b020dd8e6ceecd3ce24385ee6e9a49544e97
Freddyd95 <freddydiaz28@hotmail.com>
2024-03-27 00:19:22 -0700

    Show Route Connected

--- controllers/places.js
+++ controllers/places.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const places = require('../models/places.js')
 
+
 router.get('/', (req, res) => {
     res.render('places/index', { places })
 })
@@ -11,6 +12,16 @@ router.get('/new', (req, res) => {
 
 router.get('/:id', (req, res) => {
   res.send('GET /places')
+})
+
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render(error404)
+  }
+  else {
+  res.render('places/show', { place: places[id] })
+  }
 })
 
 router.post('/', (req, res) => {
