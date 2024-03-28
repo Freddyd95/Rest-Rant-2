d4d84812715860ce9ef7946c6c465cb77ecf1d07
Freddyd95 <freddydiaz28@hotmail.com>
2024-03-27 22:28:33 -0700

    Validation errors are working

--- models/places.js
+++ models/places.js
@@ -2,11 +2,15 @@ const mongoose = require('mongoose')
 
 const placeSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    pic: String,
+    pic: { type: String, default: 'https://images.unsplash.com/photo-1617181669950-e0602ed26dbd?q=80&w=2877&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'},
     cuisines: {type: String, required: true },
     city: { type: String, default: 'Anytown' },
     state: { type: String, default:'USA'},
-    founded: Number
+    founded: {
+        type: Number,
+        min: [1673, 'Surely not that old?!'],
+        max: [new Date().getFullYear(), 'Hey, this year is in the future!']
+    }
 })
 
 placeSchema.methods.showEstablished = function() {
